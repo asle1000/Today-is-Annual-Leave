56129cc0d970502ebc1040e2948ad1e05731003f
KyunghyunPark <asle1000@gmail.com>
2025-07-06 21:47:29 +0900

    feat: Calendar - DayCell 컴포넌트 작성

---
+++ designsystem/src/main/java/com/dayoff/designsystem/model/DayCellIndicatorType.kt
@@ -0,0 +1,13 @@
+package com.dayoff.designsystem.model
+
+/**
+ *  Created by KyunghyunPark at 2025. 7. 6.
+
+ */
+enum class DayCellIndicatorType(val label: String) {
+    NONE(""),
+    HOLIDAY("휴일"),
+    SUBSTITUTE_HOLIDAY("대체 휴일"),
+    ANNUAL_LEAVE("연차"),
+    ANNUAL_LEAVE_RECOMMEND("연차 사용 추천");
+}
