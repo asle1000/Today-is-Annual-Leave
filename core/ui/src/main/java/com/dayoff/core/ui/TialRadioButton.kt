a2623bf64fe8b1e8e0326ea4c6a097f7058c96ce
KyunghyunPark <asle1000@gmail.com>
2025-08-09 15:46:13 +0900

    feat: 라디오 버튼 및 그룹 작성(2)

--- core/ui/src/main/java/com/dayoff/core/ui/TialRadioButton.kt
+++ core/ui/src/main/java/com/dayoff/core/ui/TialRadioButton.kt
@@ -2,10 +2,12 @@ package com.dayoff.core.ui
 
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -22,6 +24,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dayoff.designsystem.theme.LocalTialColors
@@ -32,7 +36,7 @@ import com.dayoff.designsystem.theme.LocalTialTypes
 
  */
 @Composable
-fun TialRadioButton(
+fun TialTextRadioButton(
     modifier: Modifier = Modifier,
     checked: Boolean,
     title: String,
@@ -44,7 +48,8 @@ fun TialRadioButton(
 
     val shape = RoundedCornerShape(24.dp)
     val borderColor = if (checked) colors.border.brand.primary else colors.border.surface.secondary
-    val backgroundColor = if (checked) colors.background.brand.overlay else colors.background.base.white
+    val backgroundColor =
+        if (checked) colors.background.brand.overlay else colors.background.base.white
     val iconRes = if (checked) R.drawable.ic_radio_checked else R.drawable.ic_radio_unchecked
 
     Surface(
@@ -52,8 +57,7 @@ fun TialRadioButton(
             .fillMaxWidth()
             .clip(shape = shape)
             .border(width = 1.dp, color = borderColor, shape = shape)
-            .clickable(onClick = onClick),
-        color = backgroundColor
+            .clickable(onClick = onClick, role = Role.RadioButton), color = backgroundColor
     ) {
         Row(
             modifier = Modifier
@@ -84,7 +88,51 @@ fun TialRadioButton(
 }
 
 @Composable
-fun TialRadioButtonGroup(
+fun TialIconRadioButton(
+    modifier: Modifier = Modifier,
+    checked: Boolean,
+    iconRes: Int,
+    label: String,
+    onClick: () -> Unit = {},
+) {
+    val colors = LocalTialColors.current
+    val typography = LocalTialTypes.current
+
+    val shape = RoundedCornerShape(24.dp)
+    val borderColor = if (checked) colors.border.brand.primary else colors.border.surface.secondary
+    val backgroundColor = if (checked) colors.background.brand.overlay else Color.Transparent
+
+    Surface(
+        modifier = modifier
+            .clip(shape = shape)
+            .border(width = 1.dp, color = borderColor, shape = shape)
+            .clickable(onClick = onClick, role = Role.RadioButton), color = backgroundColor
+    ) {
+        Column(
+            modifier = Modifier.padding(vertical = 20.dp, horizontal = 34.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Icon(
+                painter = painterResource(id = iconRes),
+                contentDescription = "radio_button",
+                tint = Color.Unspecified,
+            )
+
+            Spacer(modifier = Modifier.height(height = 8.dp))
+
+            Text(
+                text = label,
+                style = typography.labelMedium.copy(color = colors.text.surface.primary),
+                textAlign = TextAlign.Center,
+            )
+        }
+    }
+}
+
+
+@Composable
+fun TialTextRadioButtonVerticalGroup(
     options: List<Pair<String, String>>,
     modifier: Modifier = Modifier,
     initialSelectedIndex: Int = 0,
@@ -94,19 +142,43 @@ fun TialRadioButtonGroup(
 
     Column(modifier = modifier) {
         options.forEachIndexed { index, (title, content) ->
-            TialRadioButton(
-                title = title,
-                content = content,
+            TialTextRadioButton(
+                title = title, content = content, checked = selectedIndex == index, onClick = {
+                    selectedIndex = index
+                    onSelectionChanged(index)
+                }, modifier = Modifier.padding(vertical = 4.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun TialIconRadioButtonHorizontalGroup(
+    modifier: Modifier = Modifier,
+    options: List<Pair<Int, String>>,
+    initialSelectedIndex: Int = 0,
+    onSelectionChanged: (Int) -> Unit = {}
+) {
+    var selectedIndex by remember { mutableIntStateOf(initialSelectedIndex) }
+
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        options.forEachIndexed { index, (img, label) ->
+            TialIconRadioButton(
+                iconRes = img,
+                label = label,
                 checked = selectedIndex == index,
                 onClick = {
                     selectedIndex = index
                     onSelectionChanged(index)
                 },
-                modifier = Modifier.padding(vertical = 4.dp)
             )
         }
     }
 }
+
 @Preview(showBackground = true)
 @Composable
 fun TialRadioButtonGroupPreview() {
@@ -115,9 +187,20 @@ fun TialRadioButtonGroupPreview() {
         "연차 미소진" to "사용 가능한 연차는 그대로! 기록으로만 남겨요.",
     )
 
-    TialRadioButtonGroup(
-        options = options,
-        onSelectionChanged = { _ ->
-        }
-    )
+    TialTextRadioButtonVerticalGroup(
+        options = options, onSelectionChanged = { _ ->
+        })
 }
+
+@Preview(showBackground = true)
+@Composable
+fun TialIconRadioButtonGroupPreview() {
+    val options = listOf(
+        R.drawable.img_annual_leave to "연차",
+        R.drawable.img_half_annual_leave to "반차",
+        R.drawable.img_particle_day_leave to "시간차",
+    )
+
+    TialIconRadioButtonHorizontalGroup(options = options, onSelectionChanged = { _ -> })
+}
+
