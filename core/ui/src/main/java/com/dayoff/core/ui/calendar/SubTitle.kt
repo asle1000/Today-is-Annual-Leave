f27df415aeec295df0ce9c3ddd8c8d367382a151
KyunghyunPark <asle1000@gmail.com>
2025-09-15 22:26:37 +0900

    feat: typography - 강조 처리 원복

--- core/ui/src/main/java/com/dayoff/core/ui/calendar/SubTitle.kt
+++ core/ui/src/main/java/com/dayoff/core/ui/calendar/SubTitle.kt
@@ -2,7 +2,6 @@ package com.dayoff.core.ui.calendar
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.text.font.FontWeight
 import com.dayoff.designsystem.theme.LocalTialColors
 import com.dayoff.designsystem.theme.LocalTialTypes
 
@@ -12,9 +11,7 @@ fun SubTitle(text: String) {
     val color = LocalTialColors.current
 
     Text(
-        text = text, style = style.titleLarge.copy(
-            color = color.text.surface.primary,
-            fontWeight = FontWeight.SemiBold,
-        )
+        text = text,
+        style = style.titleLarge.copy(color = color.text.surface.primary),
     )
 }
